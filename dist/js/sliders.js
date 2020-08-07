f8517c8f0aa0ed53682c6b69425d2f66fe8cb95d
VRez-web <56605107+VRez-web@users.noreply.github.com>
2020-08-07 19:53:13 +0300

    progress

--- dist/js/sliders.js
+++ dist/js/sliders.js
@@ -1,5 +1,13 @@
 $('.rooms__inner').slick({
     infinite: true,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    dots:true
   });
+
+  $('.reviews__inner').slick({
+    variableWidth:true,
+    dots:true
+  });
+
+  
